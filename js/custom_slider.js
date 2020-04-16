5e2b6db8b4a0c9beaaac94b22ffb1942899f530b
AnnaTeachMeSkills <60443531+AnnaTeachMeSkills@users.noreply.github.com>
2020-04-16 09:57:41 +0300

    Add files via upload

---
+++ js/custom_slider.js
@@ -0,0 +1,107 @@
+const defaultSettings = {
+    slideWidth: 300,
+    slideHeight: '',
+    maxSlides: 1,
+    navs:true,
+    loop: true,
+    autoplay: true,
+    timeout: 2000,
+} 
+
+
+
+let customSliderArray = document.querySelectorAll('.custom_slider');
+
+customSliderArray.forEach(item => item.customSlider = function(sliderSettings = {}) {
+    let moveNum = 0;
+    const slidesOffset = sliderSettings.slideWidth || defaultSettings.slideWidth;
+    const slideHeight = sliderSettings.slideHeight || defaultSettings.slideHeight;
+    const maxSlides = sliderSettings.maxSlides || defaultSettings.maxSlides;
+    const slides = item.querySelectorAll('div');
+    const TotalLength = slides.length * slidesOffset;
+    const navs = (sliderSettings.navs !== undefined) ? sliderSettings.navs : defaultSettings.navs;
+    const loop = (sliderSettings.loop !== undefined) ? sliderSettings.loop : defaultSettings.loop;
+    const autoplay = (sliderSettings.loop !== undefined) ? sliderSettings.autoplay : defaultSettings.autoplay;
+    const timeout = sliderSettings.timeout || defaultSettings.timeout;
+
+    buildSlider(item, slidesOffset, slideHeight, slides, navs);
+
+    let outerContainer = item.querySelector('.outer_container');
+    outerContainer.style.width = `${slidesOffset * maxSlides}px`;
+    
+    if (navs){
+        let nextSlideBtn = item.querySelector('.next_slide');
+        let prevSlideBtn = item.querySelector('.prev_slide');
+
+        nextSlideBtn.addEventListener('click', nextSlideClick);
+        prevSlideBtn.addEventListener('click', prevSlideClick);
+    }
+    
+
+    function nextSlideClick(e) {
+        e.preventDefault();
+        let customSlider = this.closest('.custom_slider');
+        let innerContainer = customSlider.querySelector('.inner_container');
+        if ( (moveNum - slidesOffset) > -TotalLength ) {
+            moveNum = moveNum - slidesOffset;
+            innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+        } else if (loop) {
+            moveNum = 0;
+            innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+        }
+
+      
+    }
+    
+    function prevSlideClick(e) {
+        e.preventDefault();
+        let customSlider = this.closest('.custom_slider');
+        let innerContainer = customSlider.querySelector('.inner_container');
+        if (moveNum < 0) {
+            moveNum = moveNum + slidesOffset;
+            innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+        } else if (loop) {
+            moveNum = - (TotalLength - slidesOffset);
+            innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+        }
+    }
+
+    if (autoplay) {
+        function moveSlide() {
+            if ( (moveNum - slidesOffset) > -TotalLength ){ 
+            moveNum = moveNum - slidesOffset;
+            let innerContainer = item.querySelector('.inner_container');
+            innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+            }
+            else  {
+                moveNum = 0;
+                let innerContainer = item.querySelector('.inner_container');
+                innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+            }
+        }
+        setInterval(moveSlide, timeout)
+    } 
+   
+})
+
+ 
+function buildSlider(slider, slideWidth, slideHeight, slides, navs) {
+    slides.forEach(item => {
+        item.classList.add('one_slide');
+        item.style.width = `${slideWidth}px`;
+        item.style.height = `${slideHeight}px`;
+    });
+
+    slider.innerHTML = `        
+        <div class="outer_container">
+            <div class="inner_container">
+                ${slider.innerHTML}
+            </div>
+        </div>`;
+        if (navs) {
+            slider.innerHTML = slider.innerHTML + `<div class="navs">
+            <a href="#" class="prev_slide"><</a>
+            <a href="#" class="next_slide">></a>
+        </div>`
+        }
+}
---
+++ js/script.js
@@ -0,0 +1,11 @@
+let main = document.querySelector('.main');
+main.customSlider({
+    maxSlides: 1,
+    slideWidth: 300,
+    slideHeight: 200,
+    navs: true,
+    loop: true,
+    autoplay: true,
+    timeout: 2000,
+});
+
